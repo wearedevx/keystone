ba6ff30513aa3567879e26590ad69cacc47c42cc
Samuel Roy <sam@pricemetry.com>
2019-12-12 11:12:28 +0100

    better keystone structure

---
+++ keystone-core/src/index.js
@@ -0,0 +1,19 @@
+const descriptor = require('./descriptor')
+const descriptorPath = require('./descriptor-path')
+const env = require('./env')
+const commands = require('./commands')
+const file = require('./file')
+const invitation = require('./invitation')
+const member = require('./member')
+const projects = require('./projects')
+
+module.exports = {
+  descriptor,
+  descriptorPath,
+  env,
+  commands,
+  file,
+  invitation,
+  member,
+  projects,
+}
