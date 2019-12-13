062aec49b28f5a4bdce950f3927301e5ef91325a
Samuel Roy <sam@pricemetry.com>
2019-12-13 11:05:48 +0100

    purgecss only for production and stage build

--- keystone-web/node.api.js
+++ keystone-web/node.api.js
@@ -3,22 +3,24 @@ const PurgecssPlugin = require('purgecss-webpack-plugin')
 const glob = require('glob')
 
 export default pluginOptions => ({
-  webpack: config => {
-    config.plugins.push(
-      new PurgecssPlugin({
-        paths: glob.sync('./src/**/*.js', { nodir: true }),
-        extractors: [
-          {
-            extractor: class {
-              static extract(content) {
-                return content.match(/[\w-/:]+(?<!:)/g) || []
-              }
+  webpack: (config, { stage }) => {
+    if (stage !== 'dev') {
+      config.plugins.push(
+        new PurgecssPlugin({
+          paths: glob.sync('./src/**/*.js', { nodir: true }),
+          extractors: [
+            {
+              extractor: class {
+                static extract(content) {
+                  return content.match(/[\w-/:]+(?<!:)/g) || []
+                }
+              },
+              extensions: ['js', 'css'],
             },
-            extensions: ['js', 'css'],
-          },
-        ],
-      })
-    )
+          ],
+        })
+      )
+    }
     return config
   },
 })
