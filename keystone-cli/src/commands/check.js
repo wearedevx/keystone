4ff6de95fb7345afdd17c8b075de943505ab794c
Kévin Quesada <kevin@Kevins-MacBook-Pro.local>
2019-12-19 16:37:59 +0100

    comme t useless commands

--- keystone-cli/src/commands/check.js
+++ keystone-cli/src/commands/check.js
@@ -1,77 +1,76 @@
-const inquirer = require('inquirer')
-const { flags } = require('@oclif/command')
-const chalk = require('chalk')
+// const inquirer = require('inquirer')
+// const { flags } = require('@oclif/command')
+// const chalk = require('chalk')
 const { CommandSignedIn } = require('../lib/commands')
-const { invite, getInvitations, getPubkey } = require('../lib/core')
 
 class CheckCommand extends CommandSignedIn {
-  async askEmail() {
-    const answer = await inquirer.prompt([
-      {
-        name: 'email',
-        type: 'input',
-        message: `What's your email address?`,
-      },
-    ])
-    return answer.email
-  }
+//   async askEmail() {
+//     const answer = await inquirer.prompt([
+//       {
+//         name: 'email',
+//         type: 'input',
+//         message: `What's your email address?`,
+//       },
+//     ])
+//     return answer.email
+//   }
 
-  async check() {
-    const userSession = await this.getUserSession()
+//   async check() {
+//     const userSession = await this.getUserSession()
 
-    try {
-      // const invitations = await getInvitations(userSession)
+//     try {
+//       // const invitations = await getInvitations(userSession)
 
-      const pubkey = await getPubkey(userSession, {
-        blockstack_id: 'l_abigael.id.blockstack',
-      })
-      console.log('pubkey', pubkey)
-    } catch (error) {
-      console.log(error)
-      this.log(`${error.message}`)
-    }
-  }
+//       const pubkey = await getPubkey(userSession, {
+//         blockstack_id: 'l_abigael.id.blockstack',
+//       })
+//       console.log('pubkey', pubkey)
+//     } catch (error) {
+//       console.log(error)
+//       this.log(`${error.message}`)
+//     }
+//   }
 
-  async run() {
-    try {
-      const { argv, flags } = this.parse(CheckCommand)
-      //at least 1 arguments required, an email
-      // const project = await this.getProjectName(flags)
-      await this.check()
-    } catch (error) {
-      this.log(error.message)
-    }
-  }
+//   async run() {
+//     try {
+//       const { argv, flags } = this.parse(CheckCommand)
+//       //at least 1 arguments required, an email
+//       // const project = await this.getProjectName(flags)
+//       await this.check()
+//     } catch (error) {
+//       this.log(error.message)
+//     }
+//   }
 }
 
-// CheckCommand.args = [
-//   {
-//     name: 'emails',
-//     required: true,            // make the arg required with `required: true`
-//     description: 'Emails for invitations to be sent', // help description
-//     hidden: false,
-//   }
-// ]
+// // CheckCommand.args = [
+// //   {
+// //     name: 'emails',
+// //     required: true,            // make the arg required with `required: true`
+// //     description: 'Emails for invitations to be sent', // help description
+// //     hidden: false,
+// //   }
+// // ]
 
-// CheckCommand.flags = {
-//   ...CommandSignedIn.flags,
-//   role: flags.string({
-//     char: 'r',
-//     multiple: false,
-//     options: ['reader', 'contributor', 'admin'],
-//     default: 'reader',
-//     description: `Assigns a role`
-//   }),
-//   removal: flags.boolean({
-//     multiple: false,
-//     default: false,
-//     description: `Deletes an invitation`
-//   })
-// }
+// // CheckCommand.flags = {
+// //   ...CommandSignedIn.flags,
+// //   role: flags.string({
+// //     char: 'r',
+// //     multiple: false,
+// //     options: ['reader', 'contributor', 'admin'],
+// //     default: 'reader',
+// //     description: `Assigns a role`
+// //   }),
+// //   removal: flags.boolean({
+// //     multiple: false,
+// //     default: false,
+// //     description: `Deletes an invitation`
+// //   })
+// // }
 
-CheckCommand.description = `Check if a blockstack id has a Keystone application public key.
-`
+// CheckCommand.description = `Check if a blockstack id has a Keystone application public key.
+// `
 
-CheckCommand.examples = [`${chalk.yellow('$ ks check example.id.blockstack')}`]
+// CheckCommand.examples = [`${chalk.yellow('$ ks check example.id.blockstack')}`]
 
 module.exports = CheckCommand
--- keystone-cli/src/commands/remove.js
+++ keystone-cli/src/commands/remove.js
@@ -1,75 +1,75 @@
-const { flags } = require('@oclif/command')
-const chalk = require('chalk')
-const fs = require('fs')
+// const { flags } = require('@oclif/command')
+// const chalk = require('chalk')
+// const fs = require('fs')
 
-const { removeProject } = require('../lib/core/project')
+// const { removeProject } = require('../lib/core/project')
 const { CommandSignedIn } = require('../lib/commands')
 
 class RemoveCommand extends CommandSignedIn {
-  async removeProject(name, force) {
-    await this.withUserSession(async userSession => {
-      try {
-        await removeProject(userSession, { name, force })
-        this.log(
-          `▻ Project ${chalk.bold(
-            name
-          )} successfully removed ${chalk.green.bold('✓')}\n`
-        )
-        const configFile = fs.readFileSync('.ksconfig')
-        const { project } = JSON.parse(configFile)
-        if (name === project) {
-          const confirm = await CommandSignedIn.confirm(
-            'Remove keystone config?'
-          )
-          if (confirm) {
-            fs.unlinkSync('.ksconfig')
-            this.log(`▻ Config file removed ${chalk.green.bold('✓')}\n`)
-          }
-        }
-      } catch (error) {
-        console.error(error)
-        this.log(`▻ ${chalk.red(error.message)}\n`)
-      }
-    })
-  }
+//   async removeProject(name, force) {
+//     await this.withUserSession(async userSession => {
+//       try {
+//         await removeProject(userSession, { name, force })
+//         this.log(
+//           `▻ Project ${chalk.bold(
+//             name
+//           )} successfully removed ${chalk.green.bold('✓')}\n`
+//         )
+//         const configFile = fs.readFileSync('.ksconfig')
+//         const { project } = JSON.parse(configFile)
+//         if (name === project) {
+//           const confirm = await CommandSignedIn.confirm(
+//             'Remove keystone config?'
+//           )
+//           if (confirm) {
+//             fs.unlinkSync('.ksconfig')
+//             this.log(`▻ Config file removed ${chalk.green.bold('✓')}\n`)
+//           }
+//         }
+//       } catch (error) {
+//         console.error(error)
+//         this.log(`▻ ${chalk.red(error.message)}\n`)
+//       }
+//     })
+//   }
 
-  async run() {
-    const { flags } = this.parse(RemoveCommand)
-    console.log(flags)
+//   async run() {
+//     const { flags } = this.parse(RemoveCommand)
+//     console.log(flags)
 
-    try {
-      const project = await this.getProjectName(flags)
-      const { force } = flags
-      await this.removeProject(project, force)
-    } catch (error) {
-      this.log(error.message)
-    }
-  }
+//     try {
+//       const project = await this.getProjectName(flags)
+//       const { force } = flags
+//       await this.removeProject(project, force)
+//     } catch (error) {
+//       this.log(error.message)
+//     }
+//   }
 }
 
-RemoveCommand.description = `Remove a project.
-...
-If you're an administrator, the project will be removed for everyone.\n
-If you're a contributor or a reader, you will be removed from the project.
-`
+// RemoveCommand.description = `Remove a project.
+// ...
+// If you're an administrator, the project will be removed for everyone.\n
+// If you're a contributor or a reader, you will be removed from the project.
+// `
 
-RemoveCommand.examples = [
-  `${chalk.yellow('$ ks remove')} ${chalk.gray.italic(
-    '#remove your project set in .ksconfig and all its files'
-  )}`,
-  `${chalk.yellow('$ ks remove --project=my-project')} ${chalk.gray.italic(
-    '#remove your project called my-project'
-  )}`,
-]
+// RemoveCommand.examples = [
+//   `${chalk.yellow('$ ks remove')} ${chalk.gray.italic(
+//     '#remove your project set in .ksconfig and all its files'
+//   )}`,
+//   `${chalk.yellow('$ ks remove --project=my-project')} ${chalk.gray.italic(
+//     '#remove your project called my-project'
+//   )}`,
+// ]
 
-RemoveCommand.flags = {
-  ...CommandSignedIn.flags,
-  force: flags.string({
-    char: 'f',
-    multiple: false,
-    description:
-      'Force the deletion. Beware, it might let some files from the project on your storage.',
-  }),
-}
+// RemoveCommand.flags = {
+//   ...CommandSignedIn.flags,
+//   force: flags.string({
+//     char: 'f',
+//     multiple: false,
+//     description:
+//       'Force the deletion. Beware, it might let some files from the project on your storage.',
+//   }),
+// }
 
 module.exports = RemoveCommand
