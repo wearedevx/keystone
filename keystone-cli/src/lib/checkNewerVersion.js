bd5f38021777a31724e07563b605122dfcb39f1c
Abigael <abigael.laldji@protonmail.com>
2020-01-06 18:29:38 +0100

    check if user version of ks is the latest

---
+++ keystone-cli/src/lib/checkNewerVersion.js
@@ -0,0 +1,24 @@
+const child_process = require('child_process')
+const chalk = require('chalk')
+
+const newestVersion = child_process
+  .execSync('npm show @keystone.sh/cli version --loglevel=error')
+  .toString()
+  .replace('\n', '')
+
+const currentVersion = child_process
+  .execSync(`ks -v | awk '{print $NR}' | awk '{split($NF,a,"/"); print a[3]}'`)
+  .toString()
+  .replace('\n', '')
+
+if (newestVersion && currentVersion && newestVersion !== currentVersion) {
+  console.log(
+    `\nVersion ${chalk.yellow(
+      newestVersion
+    )} of keystone is out. Your current version is ${chalk.yellow(
+      currentVersion
+    )}.\nConsider updating the package with : ${chalk.bold(
+      'npm update @keystone.sh/cli'
+    )}\n`
+  )
+}
