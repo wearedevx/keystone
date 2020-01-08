2697400977e182f08cdf796ad7966cb0312a685c
Abigael <abigael.laldji@protonmail.com>
2020-01-08 10:42:01 +0100

    redirect after signin with query paramters

--- keystone-web/src/pages/confirm.js
+++ keystone-web/src/pages/confirm.js
@@ -186,7 +186,7 @@ export default () => {
         <div className="my-4 flex flex-row w-2/4 justify-end">
           <div
             className="rounded font-bold text-white bg-primary py-1 px-4 shadow-md text-center cursor-pointer"
-            onClick={() => redirectToSignIn('/confirm')}
+            onClick={() => redirectToSignIn(window.location.href)}
           >
             Sign in with Blockstack
           </div>
