d521a6e1030dfb369e9acb0cf64d1bc6e8a2803f
Abigael <abigael.laldji@protonmail.com>
2020-01-17 15:11:12 +0100

    fix unjoined path

--- keystone-core/lib/file/disk.js
+++ keystone-core/lib/file/disk.js
@@ -90,12 +90,10 @@ const isFileExist = filePath => {
 
 const getModifiedFilesFromCacheFolder = (cacheFolder, absoluteProjectPath) => {
   const paths = walk.sync(cacheFolder)
-  const changes = paths.map(path => {
-    const relativePath = path.replace(cacheFolder, '')
+  const changes = paths.map(currentPath => {
+    const relativePath = currentPath.replace(cacheFolder, '')
 
-    // TODO
-    // Use path.join to create path os friendly.
-    const realPath = `${absoluteProjectPath}${relativePath}`
+    const realPath = path.join(absoluteProjectPath, relativePath)
     // does file still exist?
     if (!fs.existsSync(realPath)) {
       return {
@@ -104,8 +102,8 @@ const getModifiedFilesFromCacheFolder = (cacheFolder, absoluteProjectPath) => {
       }
     }
     // if path is not a folder, check the content
-    if (fs.lstatSync(path).isFile()) {
-      const cacheContent = fs.readFileSync(path)
+    if (fs.lstatSync(currentPath).isFile()) {
+      const cacheContent = fs.readFileSync(currentPath)
       const content = fs.readFileSync(realPath)
       if (hash(cacheContent) !== hash(content)) {
         return {
