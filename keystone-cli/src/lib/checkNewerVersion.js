ec65eef4b1da922c2581e949e5f8a8ac1bcae270
Abigael <abigael.laldji@protonmail.com>
2020-02-24 16:34:54 +0100

    fix update checker

--- keystone-cli/src/lib/checkNewerVersion.js
+++ keystone-cli/src/lib/checkNewerVersion.js
@@ -8,13 +8,15 @@ module.exports = () =>
   checkForUpdate(pkg, {
     interval: 3600000, // For how long to cache latest version (default: 1 day)
   })
-    .then(({ latest }) => {
-      console.log(
-        `${chalk.bgRed('UPDATE AVAILABLE')} version ${chalk.yellow(
-          latest
-        )} is out. Run ${chalk.blue(
-          `npm i -g '@keystone.sh/cli@latest'`
-        )} to install it.`
-      )
+    .then(result => {
+      if (result) {
+        console.log(
+          `${chalk.bgRed('UPDATE AVAILABLE')} version ${chalk.yellow(
+            result.latest
+          )} is out. Run ${chalk.blue(
+            `npm i -g '@keystone.sh/cli@latest'`
+          )} to install it.`
+        )
+      }
     })
     .catch(err => console.error(`Failed to check for updates: ${err}`))
