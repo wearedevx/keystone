5590266669f11e13b9712c2bdd67c3d6425e41e8
Samuel Roy <sam@pricemetry.com>
2020-03-08 18:11:15 +0100

    add favicons

--- keystone-web/static.config.js
+++ keystone-web/static.config.js
@@ -1,6 +1,36 @@
 import path from 'path'
+import React from 'react'
 
 export default {
+  Document: ({ Html, Head, Body, children }) => (
+    <Html>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2d2d2d" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+
   productionSourceMaps: true,
   devServer: {
     port: 8000,
