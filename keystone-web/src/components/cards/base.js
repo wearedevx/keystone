4e2b24dc8c92e8604b2481102f05082d0f831f28
Samuel Roy <sam@pricemetry.com>
2020-03-16 14:50:50 +0100

    fix signin in development mode plus design fixes

--- keystone-web/src/components/cards/base.js
+++ keystone-web/src/components/cards/base.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default ({ title, children }) => {
   return (
-    <div className="shadow-md rounded p-4 bg-white w-2/4">
-      <h2 className="text-xl mb-4">{title}</h2>
+    <div className="p-4 bg-white w-2/4 text-center">
+      <h2 className="text-xl mb-4 font-mono">{title}</h2>
       <div>{children}</div>
     </div>
   )
