cb74e876ebaf90cd31cdc8d4bfa822cabbb57351
Abigael <abigael.laldji@protonmail.com>
2020-03-20 18:01:40 +0100

    remove useless commands

--- keystone-cli/src/commands/list.js
+++ keystone-cli/src/commands/list.js
@@ -3,57 +3,24 @@ const { flags } = require('@oclif/command')
 const chalk = require('chalk')
 
 const {
-  listEnvironments,
-  listAllMembers,
-  listEnvMembers,
   listAllFiles,
   listEnvFiles,
-  listProjects,
 } = require('@keystone.sh/core/lib/commands/list')
 
 const { CommandSignedIn } = require('../lib/commands')
 
 class ListCommand extends CommandSignedIn {
   async run() {
-    const { flags, args } = this.parse(ListCommand)
+    const { flags } = this.parse(ListCommand)
 
     await this.withUserSession(async userSession => {
-      if (args.type === 'projects') {
-        const projects = await listProjects(userSession)
-        if (projects && projects.length > 0) {
-          projects.forEach(project => {
-            this.log(
-              `> ${project.name.split('/')[0]}/${chalk.grey(
-                project.name.split('/')[1]
-              )}`
-            )
-          })
-        } else {
-          this.log('No project Found in user workspace!')
-        }
-      } else {
-        const env = await this.getProjectEnv()
-        const project = await this.getProjectName()
+      const env = await this.getProjectEnv()
+      const project = await this.getProjectName()
 
-        if (args.type === 'members') {
-          if (flags.all) {
-            listAllMembers(userSession, { project })
-          } else {
-            listEnvMembers(userSession, {
-              project,
-              env,
-              isProjectMembers: false,
-            })
-          }
-        } else if (args.type === 'files') {
-          if (flags.all) {
-            listAllFiles(userSession)
-          } else {
-            listEnvFiles(userSession, { project, env })
-          }
-        } else if (args.type === 'environments') {
-          listEnvironments(userSession, { project })
-        }
+      if (flags.all) {
+        await listAllFiles(userSession)
+      } else {
+        await listEnvFiles(userSession, { project, env })
       }
     })
   }
@@ -62,23 +29,7 @@ class ListCommand extends CommandSignedIn {
 ListCommand.description = `Lists projects, environments, members and files
 `
 
-ListCommand.examples = [
-  chalk.blue('$ ks list members'),
-  chalk.blue('$ ks list members --all'),
-  chalk.blue('$ ks list projects'),
-  chalk.blue('$ ks list environments'),
-  chalk.blue('$ ks list files'),
-]
-
-ListCommand.args = [
-  {
-    name: 'type',
-    required: true, // make the arg required with `required: true`
-    description:
-      'What do you want to list (projects, environments, members or files)', // help description
-    hidden: false,
-  },
-]
+ListCommand.examples = [chalk.blue('$ ks list files')]
 
 ListCommand.flags = {
   ...CommandSignedIn.flags,
