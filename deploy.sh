e6a826da0cf7e90368ebae6b0b6a62735d014f2a
gaelph <contact@gael-philippe.fr>
2020-09-16 17:23:20 +0200

    build(functions): update dependencies

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 WORK=$PWD
 
 cd $PWD/functions/ksauth
+go get -u github.com/wearedevx/keystone@go
 sh ./deploy.sh
 
 cd $WORK
 
 cd $PWD/functions/ksapi
+go get -u github.com/wearedevx/keystone@go
 sh ./deploy.sh
 
 cd $WORK
