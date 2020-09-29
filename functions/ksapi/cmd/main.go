066ef22be10a71b9cd02c6cff1cbeb0ecb6a5428
gaelph <contact@gael-philippe.fr>
2020-09-29 20:18:46 +0200

    refactor(login): use interface for third party auth

--- functions/ksapi/cmd/main.go
+++ functions/ksapi/cmd/main.go
@@ -7,12 +7,12 @@ import (
 	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
-	. "github.com/wearedevx/keystone/functions/ksapi"
+	"github.com/wearedevx/keystone/functions/ksapi"
 )
 
 func main() {
 	ctx := context.Background()
-	if err := funcframework.RegisterHTTPFunctionContext(ctx, "/", UserService); err != nil {
+	if err := funcframework.RegisterHTTPFunctionContext(ctx, "/", ksapi.UserService); err != nil {
 		log.Fatalf("funcframework.RegisterHTTPFunctionContext: %v\n", err)
 	}
 	// Use PORT environment variable, or default to 8080.
