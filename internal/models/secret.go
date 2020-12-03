8009211a22ba127a14a3f005aa7def678e484fa3
gaelph <contact@gael-philippe.fr>
2020-12-03 10:46:05 +0100

    fix(secret model): add type string or file

--- internal/models/secret.go
+++ internal/models/secret.go
@@ -8,9 +8,17 @@ import (
 	"gorm.io/gorm"
 )
 
+type SecretType string
+
+const (
+	SecretString SecretType = "string"
+	SecretFile              = "file"
+)
+
 type Secret struct {
 	gorm.Model
-	Name string `json:"name" gorm:"not null"`
+	Name string     `json:"name" gorm:"not null"`
+	Type SecretType `json:"type" gorm:"not null"`
 }
 
 func (u *Secret) Deserialize(in io.Reader) error {
