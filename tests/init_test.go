69b7fc082a4cea84694f8badae754e7e03782901
Abigael <abigael@wearedevx.com>
2021-04-13 17:58:24 +0200

    feat(test) setup functin

--- tests/init_test.go
+++ tests/init_test.go
@@ -1,19 +1,69 @@
 package main
 
 import (
+	"os"
+	"path"
+	"testing"
+
 	"github.com/rogpeppe/go-internal/testscript"
 	"github.com/wearedevx/keystone/cmd"
-	"os"
-	"testing"
+	"github.com/wearedevx/keystone/internal/config"
+	. "github.com/wearedevx/keystone/internal/models"
+	"github.com/wearedevx/keystone/internal/repo"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(testscript.RunMain(m, map[string]func() int{
-		"toto": cmd.Execute,
+		"ks": cmd.Execute,
 	}))
 }
-func TestHelloWorld(t *testing.T) {
+
+func SetupFunc(env *testscript.Env) error {
+	Repo := new(repo.Repo)
+	Repo.Connect()
+
+	var user1 *User = &User{
+		ExtID:       "56883564",
+		UserID:      "00fb7666-de43-4559-b4e4-39b172117dd8",
+		AccountType: "github",
+		Username:    "LAbigael",
+		Fullname:    "Test user1",
+		Email:       "abigael.laldji@protonmail.com",
+	}
+
+	Repo.GetOrCreateUser(user1)
+
+	user1Account := map[string]string{
+		"account_type": "github",
+		"email":        "abigael.laldji@protonmail.com",
+		"ext_id":       "56883564",
+		"fullname":     "Michel",
+		"user_id":      "00fb7666-de43-4559-b4e4-39b172117dd8",
+		"username":     "LAbigael",
+		"Fullname":     "Test user1",
+	}
+
+	// Set home dir for test
+	env.Setenv("HOME", path.Join(env.Getenv("WORK"), "home"))
+	// Set home to test's dir to init config for the test
+	os.Setenv("HOME", path.Join(env.Getenv("WORK"), "home"))
+
+	// Create config folder
+	os.MkdirAll(path.Join(os.Getenv("HOME"), ".config"), 0777)
+
+	config.InitConfig("")
+
+	config.AddAccount(user1Account)
+	config.SetCurrentAccount(0)
+	config.Write()
+
+	return nil
+}
+
+func TestInitCommand(t *testing.T) {
+
 	testscript.Run(t, testscript.Params{
-		Dir: ".",
+		Dir:   "./init/",
+		Setup: SetupFunc,
 	})
 }
