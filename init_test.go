2b8c997dd651b32def7634cc263040c24aab6e40
gaelph <contact@gael-philippe.fr>
2021-04-15 12:02:40 +0200

    tests

--- init_test.go
+++ init_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 	"github.com/wearedevx/keystone/cmd"
+	. "github.com/wearedevx/keystone/internal/jwt"
+	. "github.com/wearedevx/keystone/internal/models"
+	"github.com/wearedevx/keystone/internal/repo"
 )
 
 func TestMain(m *testing.M) {
@@ -15,16 +19,63 @@ func TestMain(m *testing.M) {
 		"ks": cmd.Execute,
 	}))
 }
-func Test_ExecuteCommand(t *testing.T) {
+
+func SetupFunc(env *testscript.Env) error {
+	Repo := new(repo.Repo)
+	Repo.Connect()
+
+	var user1 *User = &User{
+		ExtID:       "56883564",
+		UserID:      "00fb7666-de43-4559-b4e4-39b172117dd8",
+		AccountType: "github",
+		Username:    "LAbigael",
+		Fullname:    "Test user1",
+		Email:       "abigael.laldji@protonmail.com",
+	}
+
+	token, _ := MakeToken(*user1)
+
+	Repo.GetOrCreateUser(user1)
+
+	homeDir := path.Join(env.Getenv("WORK"), "home")
+	configDir := path.Join(homeDir, ".config")
+	pathToKeystoneFile := path.Join(configDir, "keystone.yaml")
+
+	// Set home dir for test
+	env.Setenv("HOME", homeDir)
+	log.Println("HOME ?", env.Getenv("HOME"))
+
+	// Create config folder
+	err := os.MkdirAll(configDir, 0777)
+
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(pathToKeystoneFile, []byte(`
+accounts:
+- Fullname: Test user1
+  account_type: github
+  email: abigael.laldji@protonmail.com
+  ext_id: "56883564"
+  fullname: Michel
+  user_id: 00fb7666-de43-4559-b4e4-39b172117dd8
+  username: LAbigael
+auth_token: `+token+`
+current: 0
+`), 0o777)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestInitCommand(t *testing.T) {
+
 	testscript.Run(t, testscript.Params{
-		Dir: "./tests",
-		Setup: func(env *testscript.Env) error {
-			fmt.Println(env.Getenv("WORK"))
-			fmt.Println(env.WorkDir)
-
-			env.Setenv("HOME", path.Join(env.Getenv("WORK"), "home"))
-
-			return nil
-		},
+		Dir:   "./init/",
+		Setup: SetupFunc,
 	})
 }
