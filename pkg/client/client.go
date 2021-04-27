4ffc14f022b330d0153b3d76bf44595d4b7f6bc2
gaelph <contact@gael-philippe.fr>
2021-04-27 16:51:25 +0200

    feat(): member and member add and whoami

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -19,6 +19,11 @@ var ksapiURL string  //= "http://localhost:9001"
 
 type KeystoneClient interface {
 	InitProject(name string) (Project, error)
+	// Members
+	ProjectMembers(projectID string) ([]ProjectMember, error)
+	ProjectAddMembers(projectID string, members map[string]map[string]string) error
+	ProjectRemoveMembers(projectID string, members []string) error
+	MemberSetRole(memberId string, role string)
 }
 
 func getLoginRequest() (LoginRequest, error) {
