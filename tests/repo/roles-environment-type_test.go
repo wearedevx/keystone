7a474adc45cfb8e002de63e2a726b4f103feedcb
Kévin <kevin@wearedevx.com>
2021-05-03 17:16:46 +0200

    test(rights) test with db rights with dev user

--- tests/repo/roles-environment-type_test.go
+++ tests/repo/roles-environment-type_test.go
@@ -29,4 +29,10 @@ func TestEnvType(t *testing.T) {
 	// repo RightsRepo, user *User, project *Project, environment *Environment
 	can, _ := rights.CanUserReadEnvironment(Repo, &user, &project, &environment)
 	assert.True(t, can, "Oops! User "+user.Username+" shoud be able to read on "+environment.Name+" environment")
+
+	can, _ = rights.CanUserWriteOnEnvironment(Repo, &user, &project, &environment)
+	assert.True(t, can, "Oops! User "+user.Username+" shoud be able to write on "+environment.Name+" environment")
+
+	can, _ = rights.CanUserInviteOnEnvironment(Repo, &user, &project, &environment)
+	assert.False(t, can, "Oops! User "+user.Username+" can't invite on "+environment.Name+" environment")
 }
