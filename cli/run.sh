207dcc28f0656c039bc5057ab7f8a0330f920cde
gaelph <contact@gael-philippe.fr>
2021-05-06 14:57:02 +0200

    dev(run): correct ApiUrl env variable

--- cli/run.sh
+++ cli/run.sh
@@ -2,6 +2,6 @@
 
 export $(cat .env-dev | xargs)
 
-LDFLAGS="-X github.com/wearedevx/keystone/cli/pkg/client.ksapiURL=$KSAPI_URL"
+LDFLAGS="-X github.com/wearedevx/keystone/cli/pkg/client.ApiURL=$KSAPI_URL"
 
 go run -ldflags "$LDFLAGS" main.go "$@"
