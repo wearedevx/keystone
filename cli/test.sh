e6f87e1105099e071614b269ff7466a15e0c95ff
Kévin <kevin@wearedevx.com>
2021-05-07 10:56:08 +0200

    test(start) clean start shell

--- cli/test.sh
+++ cli/test.sh
@@ -19,53 +19,12 @@ cd ../api
 go run -tags test main.go &
 
 cd ../cli
-# Dpesn't work, with one file name with "-v" param
-# # If no test file given, test all files.
-# FOLDERTOTEST = $@
-
-# if [ -z "$FOLDERTOTEST" ]; then
-#     FOLDERTOTEST="./..."
-# fi
-
-# # Start test
-# go test -tags test -ldflags "$LDFLAGS" -work "$FOLDERTOTEST"
-
-echo "START TEST"
 
 echo "go test -tags test -ldflags \"$LDFLAGS\" -work $@"
 go test -tags test -ldflags "$LDFLAGS" -work "$@"
 
-EXIT_STATUS_CODE=$?
-
-echo "FINISH TEST WITH $EXIT_STATUS_CODE"
-
-function removeProcessId() {
-    kspidfile=$1
-    ksapidpath=${TMPDIR}${kspidfile}
-
-    # Check if gcloud auth func pid exist
-    if [ -f "$ksapidpath" ]; then
-
-        echo "rm $kspidfile"
-
-        pid=$(cat $ksapidpath)
-        
-        echo "kill $kspidfile, PID=$pid"
-
-        rm $ksapidpath
-
-        kill -- -$pid
-
-    else
-        echo "File not found: $ksapidpath"
-    fi
-}
-
 removeProcessId "keystone_ksapi.pid"
 
-# Delete db file
-# echo "rm $DBFILE"
-# rm $DBFILE
 rm "/tmp/keystone_gorm"*
 
 kill -9 $(lsof -t -i:9001)
