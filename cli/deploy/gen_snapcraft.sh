3660bae3507e17deb3796d8f4f48e22d681c912f
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-07-07 17:33:38 +0200

    fix deploy snap auth proxy

--- cli/deploy/gen_snapcraft.sh
+++ cli/deploy/gen_snapcraft.sh
@@ -8,7 +8,7 @@ sed -i "s#<%BRANCH%>#${BRANCH}#g" $TARGET
 sed -i "s#<%VERSION%>#${VERSION}#g" $TARGET
 
 sed -i "s#<%KSAPI_URL%>#${KSAPI_URL}#g" $TARGET
-sed -i "s#<%AUTH_PROXY%>#${PROXY_AUTH}#g" $TARGET
+sed -i "s#<%AUTH_PROXY%>#${AUTH_PROXY}#g" $TARGET
 
 sed -i "s#<%GITHUB_CLIENT_ID%>#${GITHUB_CLIENT_ID}#g" $TARGET
 sed -i "s#<%GITHUB_CLIENT_SECRET%>#${GITHUB_CLIENT_SECRET}#g" $TARGET
