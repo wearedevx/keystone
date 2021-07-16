e3aa3dc2ce33d5f7e958819d42b93b1da1972be1
Abigael <abigael@wearedevx.com>
2021-07-16 15:57:56 +0200

    fix(ci setup) command description

--- cli/cmd/ci_setup.go
+++ cli/cmd/ci_setup.go
@@ -10,13 +10,10 @@ import (
 // setupCmd represents the setup command
 var setupCmd = &cobra.Command{
 	Use:   "setup",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Setup CI service settings to push secrets.",
+	Long: `Use this command to modify CI service specific settings
+like API key and project name.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		ciService, err := SelectCiService(*ctx)
@@ -27,7 +24,7 @@ to quickly create a Cobra application.`,
 		}
 
 		ciService = ciService.Setup()
-		// ciService = askForApiKey(ciService)
+
 		if ciService.Error() != nil {
 			ui.PrintError(ciService.Error().Error())
 			os.Exit(1)
