565e6a81f3ff5d4454d1ecceddd2596f5100ed14
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-08-11 11:43:31 +0200

    fix(): extraneous println

--- cli/internal/config/config.go
+++ cli/internal/config/config.go
@@ -171,8 +171,6 @@ func FindAccount(c auth.AuthService) (user models.User, current int) {
 
 // Create conf file if not exist
 func createFileIfNotExist(filePath string) {
-
-	fmt.Println(filePath)
 	// Check if need to create file
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		// path/to/whatever does not exist
