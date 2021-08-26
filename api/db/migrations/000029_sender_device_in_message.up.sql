b51ee3d567e00b9f0eb5a63c032113c915a24d87
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-08-26 10:06:14 +0200

    fix(migration): remove public keys before adding uid column

--- api/db/migrations/000029_sender_device_in_message.up.sql
+++ api/db/migrations/000029_sender_device_in_message.up.sql
@@ -1,14 +1,14 @@
-truncate table messages;
+TRUNCATE TABLE messages;
 
 ALTER TABLE public.messages
 DROP CONSTRAINT fk_messages_public_key;
 
-alter table messages rename column public_key_id to recipient_device_id;
+ALTER TABLE messages RENAME COLUMN public_key_id TO recipient_device_id;
 
 ALTER TABLE public.messages
 ADD CONSTRAINT fk_messages_recipient_device_id FOREIGN KEY (recipient_device_id) REFERENCES public.devices(id);
 
-alter table messages ADD column sender_device_id int not null;
+ALTER TABLE messages ADD COLUMN sender_device_id int NOT NULL;
 
 ALTER TABLE public.messages
 ADD CONSTRAINT fk_messages_sender_device_id FOREIGN KEY (sender_device_id) REFERENCES public.devices(id);
