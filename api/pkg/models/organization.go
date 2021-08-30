797d187b5ec8a98bb83a149e5c87cd6a71176759
Abigael <abigael@wearedevx.com>
2021-08-30 10:52:45 +0200

    feat(orga) models and migration

---
+++ api/pkg/models/organization.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"encoding/json"
+	"io"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Organization struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	OwnerID   uint      `json:"owner_id" gorm:"uniqueIndex:idx_organizations_owner_id"`
+	Name      string    `json:"name"`
+	Paid      bool      `json:"paid"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (pm *Organization) BeforeCreate(tx *gorm.DB) (err error) {
+	pm.CreatedAt = time.Now()
+	pm.UpdatedAt = time.Now()
+
+	return nil
+}
+
+func (pm *Organization) BeforeUpdate(tx *gorm.DB) (err error) {
+	pm.UpdatedAt = time.Now()
+
+	return nil
+}
+
+func (pm *Organization) Deserialize(in io.Reader) error {
+	return json.NewDecoder(in).Decode(pm)
+}
+
+func (pm *Organization) Serialize(out *string) (err error) {
+	var sb strings.Builder
+
+	err = json.NewEncoder(&sb).Encode(pm)
+
+	*out = sb.String()
+
+	return err
+}
