246ec483ad4302c62cc033308755731285f78ee2
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-09-07 16:47:57 +0200

    feat(healthcheck): add the route

---
+++ api/controllers/healthcheck.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func GetHealthCheck(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	http.Error(w, "", http.StatusOK)
+}
