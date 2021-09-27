a1f134358fb96b8b96d25da942aa929be365d7e3
Abigael <abigael@wearedevx.com>
2021-09-27 11:00:33 +0200

    fix orga rename description

--- cli/cmd/orga_rename.go
+++ cli/cmd/orga_rename.go
@@ -29,13 +29,8 @@ import (
 // renameCmd represents the rename command
 var renameCmd = &cobra.Command{
 	Use:   "rename",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Rename an organization",
+	Long:  `Rename an organization.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		argc := len(args)
 		if argc != 2 {
