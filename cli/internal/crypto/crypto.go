af3d0c7d9b77020c11c406105dd332e7defd7a90
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-10-26 12:16:25 +0200

    refactor the backup/restore into the archive package

--- cli/internal/crypto/crypto.go
+++ cli/internal/crypto/crypto.go
@@ -43,6 +43,10 @@ func DecryptMessage(recipientPrivateKey []byte, senderPublicKey []byte, msg []by
 	return p, nil
 }
 
+// Encrypts a file using a user-provided passphrase.
+// `filepath` is the path to the file to be encrypted, and
+// `passphrase` is the user-provided passphrase.
+// It returns the encrypted content and an error.
 func EncryptFile(filepath, passphrase string) (encrypted []byte, err error) {
 	contents, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -62,4 +66,35 @@ func EncryptFile(filepath, passphrase string) (encrypted []byte, err error) {
 	}
 
 	return encrypted, nil
+}
+
+// Attempbs to decrypt a file using a user-porvided passphrase
+// `filepath` is the path to the encrypted file
+// `target` is the path to output the decrypted content
+// `passphrase` is the user-provided passphrase
+// It returns the decrypted content and an error
+func DecryptFile(filepath, target, passphrase string) (err error) {
+	contents, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	scell, err := cell.SealWithPassphrase(passphrase)
+	if err != nil {
+		return err
+	}
+
+	decrypted, err := scell.Decrypt([]byte(contents), nil)
+	if err != nil {
+		return err
+	}
+
+	decrypted, err = base64.StdEncoding.DecodeString(string(decrypted))
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(target, decrypted, 0600)
+
+	return err
 }
