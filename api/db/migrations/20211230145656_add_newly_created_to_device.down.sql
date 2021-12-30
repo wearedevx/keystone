52b66c37b2c6a7d3dd396e8ffa1fac0b72774f89
Abigael <abigael@wearedevx.com>
2021-12-30 16:51:23 +0100

    email are sent outside of controller

---
+++ api/db/migrations/20211230145656_add_newly_created_to_device.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.devices
+  DROP COLUMN IF EXISTS newly_created;
---
+++ api/pkg/notification/notification.go
@@ -0,0 +1,55 @@
+package notification
+
+import (
+	"fmt"
+
+	"github.com/wearedevx/keystone/api/internal/emailer"
+	"github.com/wearedevx/keystone/api/pkg/models"
+	"github.com/wearedevx/keystone/api/pkg/repo"
+)
+
+// Send email to amdins and users for every new devices created
+func SendEmailForNewDevices(r repo.IRepo) error {
+	var newDevices []models.Device
+	r.GetNewlyCreatedDevices(&newDevices)
+	for _, device := range newDevices {
+		// Newly created devices only have one user
+		user := device.Users[0]
+
+		var adminProjectsMap map[string][]string
+		if err := r.GetAdminsFromUserProjects(user.ID, &adminProjectsMap).Err(); err != nil {
+			return err
+		}
+		for adminEmail, projectList := range adminProjectsMap {
+			// Send mail to admins of user projects
+			e, err := emailer.NewDeviceAdminMail(user.Username, projectList, device.Name)
+			if err != nil {
+				return err
+			}
+
+			if err = e.Send([]string{adminEmail}); err != nil {
+				fmt.Printf("Add New Device Admin Mail err: %+v\n", err)
+				return err
+			}
+		}
+
+		// Send mail to user
+		e, err := emailer.NewDeviceMail(device.Name, user.Username)
+		if err != nil {
+			return err
+		}
+
+		if user.Email != "" {
+			if err = e.Send([]string{user.Email}); err != nil {
+				fmt.Printf("Add New Device User Mail err: %+v\n", err)
+				return err
+			}
+		}
+		device.NewlyCreated = false
+		if err = r.GetDb().Save(&device).Error; err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
